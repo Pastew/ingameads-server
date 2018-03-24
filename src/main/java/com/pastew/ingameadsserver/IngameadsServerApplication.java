f40a3df5cf62cab5d24fd390d61b31dcaf606558
Pastew <kpastew@gmail.com>
2018-03-24 14:55:25 +0100

    Empty project from Spring Initialzr

---
+++ src/main/java/com/pastew/ingameadsserver/IngameadsServerApplication.java
@@ -0,0 +1,12 @@
+package com.pastew.ingameadsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngameadsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngameadsServerApplication.class, args);
+	}
+}
