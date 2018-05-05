074ac97bd5ced35bee426593c06be9c77180fbdb
Pastew <kpastew@gmail.com>
2018-05-05 18:09:49 +0200

    Moved properties to remote config server and it works!

--- ingameads-ui/src/main/java/com/pastew/ingameadsui/dev/DevRestController.java
+++ ingameads-ui/src/main/java/com/pastew/ingameadsui/dev/DevRestController.java
@@ -2,15 +2,17 @@ package com.pastew.ingameadsui.dev;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
+@RestController
 public class DevRestController {
 
     @Value("${message:Hello default}")
     private String message;
 
     @RequestMapping("/message")
-    String getMessage(){
+    public String getMessage(){
         return this.message;
     }
 }
