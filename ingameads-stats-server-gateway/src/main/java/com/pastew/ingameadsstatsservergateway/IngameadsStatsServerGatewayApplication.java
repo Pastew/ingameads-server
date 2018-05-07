831a8b342991d206988b89f65a6ad229741e468d
Pastew <kpastew@gmail.com>
2018-05-07 20:48:38 +0200

    Added empty project for stats server

---
+++ ingameads-stats-server-gateway/src/main/java/com/pastew/ingameadsstatsservergateway/IngameadsStatsServerGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.pastew.ingameadsstatsservergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngameadsStatsServerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngameadsStatsServerGatewayApplication.class, args);
+	}
+}
